dc65e170d9c598d6f9fa175fdd47d2e613e5ac0b
Harsh Pandit <harsh.pandit2004@gmail.com>
2023-03-31 07:46:30 +0530

    revert the goddamned commit motherfuc...

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,15 +7,6 @@ export default function Navbar() {
         <tr>
           <td>
             <a
-              href="https://www.linkedin.com/in/harsh-pandit-4b1013235/"
-              class="stylisethisbutton"
-              title="LinkedIn"
-            >
-              <i class="fab fa-linkedin"></i>
-            </a>
-          </td>
-          <td>
-            <a
               href="https://github.com/harshpandit2004/bookinator-frontend"
               class="stylisethisbutton"
               title="Github"
